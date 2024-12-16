3796664454549ed207cb27ff7ecc812abb15795c
Simeon <simeonogunyinka@gmail.com>
2024-12-16 21:59:44 +0100

    deployed on vercel

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,16 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: {
-        rejectUnauthorized: false, // Use true if your database requires strict SSL validation
-      },
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'estate_kings_basketball'),
+      user: env('DATABASE_USERNAME', 'estate_kings_basketball_user'),
+      password: env('DATABASE_PASSWORD', 'fXbHdh910kVPY5lt1Q7ShMCjDfV9CNtQ'),
+      ssl: env.bool('DATABASE_SSL', false), // Set to true if your database requires SSL
     },
-    debug: false,
+    pool: {
+      min: 2,
+      max: 10,
+    },
   },
 });
